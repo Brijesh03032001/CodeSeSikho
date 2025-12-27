459e359d67ea6820f0006acf00721b04493c0a66
Brijesh03032001 <sainibrijesh01@gmail.com>
2025-12-27 14:54:41 -0700

    feat: Update Tutorials page to use TutorialsContent component

--- app/tutorials/page.tsx
+++ app/tutorials/page.tsx
@@ -1,10 +1,5 @@
+import TutorialsContent from "@/components/TutorialsContent";
+
 export default function TutorialsPage() {
-  return (
-    <div className="pt-20 min-h-screen">
-      <div className="max-w-7xl mx-auto px-6 py-12">
-        <h1 className="text-4xl font-bold mb-4">Tutorials</h1>
-        <p className="text-xl text-gray-400">Coming soon...</p>
-      </div>
-    </div>
-  );
+  return <TutorialsContent />;
 }
